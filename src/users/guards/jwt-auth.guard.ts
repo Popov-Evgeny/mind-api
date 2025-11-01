fd363bc7309bb917a272392fb0d4916558334c60
Popov Evgenii <ev.popov@elma-bs.ru>
2025-11-01 23:36:22 +0600

    Code cleaning

--- src/users/guards/jwt-auth.guard.ts
+++ src/users/guards/jwt-auth.guard.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
-    console.log(context);
     return super.canActivate(context);
   }
 }
